a096b32983c0b632aef3ad685e78900d0d3566fc
Mari Garman <garman.mariana@gmail.com>
2019-05-29 00:16:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ end
     song
   end
 
-  def Song.create_from_filename(filename)
+def Song.create_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
